af650671c74007bddb93b3f82b5052d864c4313e
Roel Hogervorst <hogervorst.rm@gmail.com>
2017-12-29 10:27:26 +0100

    cmdline version works

--- R/cmdline_test.R
+++ R/cmdline_test.R
@@ -1,11 +1,13 @@
 #!/usr/bin/Rscript --vanilla
 args <- commandArgs(trailingOnly = TRUE)
 if (length(args) < 1){
-    stop("I think you forgot to input an image and output name? \n")
+    stop("I think you forgot to input an output name? \n")
 }
 
 ### cmdline testing
 ## take only name and return a bananagif with that name
 banana <- magick::image_read("../images/banana.gif")
 message("writing a banana to: ", args[1])
-magick::image_write(image = banana, path = args[1])
+magick::image_write(image = banana, path = args[[1]])
+
+
